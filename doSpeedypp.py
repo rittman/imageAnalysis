945607447a0d9da6ded26012e1f9c07a639b1dd4
Tim Rittman <tr332@medschl.cam.ac.uk>
2014-09-06 10:17:13 +0100

    Update doSpeedypp.py
    Added comments.

--- doSpeedypp.py
+++ doSpeedypp.py
@@ -1,19 +1,35 @@
+"""
+This script is designed to submit analysis of functional images to the grid engine at the WBIC for preprocessing.
+
+The main preprocessing function called is speedypp.py from the signal processing toolbox.
+"""
 import glob,os
 from os import path
 
+email="tr332@medschl.cam.ac.uk"
+
+# define diagnostic groups here
 diags = ["Control"] #"Control",
+
+# Get the name of the current working directory
 curDir = os.getcwd()
 
+# Iterate through the diagnostic groups
 for diag in diags:
+	# get a list of all the directories (ie subjects) for each group
 	dirs = [ v for v in glob.glob(path.join(curDir,diag,"*")) if path.isdir(v) ]
+	
+	# iterate through the subject directories
 	for d in dirs:
-		os.chdir(d)
-		f = open("submitScript.sh","wb")
-		f.writelines('\n'.join(["#!/bin/bash","#$ -cwd","#$ -l qname=clusterall.q","#$ -M tr332@medschl.cam.ac.uk","#$ -V"]))
-		f.writelines("\nfslreorient2std functional.nii functional_reordered.nii\n")
-		f.writelines("\nfslreorient2std structural.nii structural_reordered.nii\n")
-		f.writelines("\n3dWarp -prefix structural_reordered_deob.nii -deoblique structural_reordered.nii\n")
+		os.chdir(d)  # move in to subject's directory
+		f = open("submitScript.sh","wb")  # open a script to submit the preprocessing
+		f.writelines('\n'.join(["#!/bin/bash","#$ -cwd","#$ -l qname=clusterall.q","#$ -M "+email,"#$ -V"])) # headers required for the grid engine
+		f.writelines("\nfslreorient2std functional.nii functional_reordered.nii\n") # reorientate the functional image so that it applies skull strip and displays correctly
+		f.writelines("\nfslreorient2std structural.nii structural_reordered.nii\n") # reorientate structural image
+		f.writelines("\n3dWarp -prefix structural_reordered_deob.nii -deoblique structural_reordered.nii\n") # deoblique the structural image
+		
+		# speedypp with options - these may need changing, see signal processing toolbox for details
 		f.writelines("\n~/fmri_spt/speedypp.py -d functional_reordered.nii -a structural_reordered.nii -o -f 8mm --rall --skullstrip --atrophy --betthresh=0.2 --tpattern=seqminus --highpass=0.01 --despike --TR=2 --basetime=10 --OVERWRITE\n")
-		f.close()
-		os.system("qsub submitScript.sh")
-		os.chdir(curDir)
+		f.close() # close submission file
+		os.system("qsub submitScript.sh") # submit submision file to the grid engine
+		os.chdir(curDir) # move back to the main directory
