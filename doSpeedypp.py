c44915bd3dbed90f20489c06503de0c0b58942ad
Robin Borchert <rb729@wbic0.(none)>
2014-09-08 13:44:20 +0100

    initial changes to motion correction

--- doSpeedypp.py
+++ doSpeedypp.py
@@ -10,27 +10,37 @@ from os import path
 email="tr332@medschl.cam.ac.uk"
 
 # define diagnostic groups here
-diags = ["Control"] #"Control",
+diags = ["Control","PD"] #"Control",
 
 # Get the name of the current working directory
 curDir = os.getcwd()
 
 # Iterate through the diagnostic groups
-for diag in diags:
+for diag in [diags[0]]: #delete [0] when running the test properly, this is just for the test. 
 	# get a list of all the directories (ie subjects) for each group
-	dirs = [ v for v in glob.glob(path.join(curDir,diag,"*")) if path.isdir(v) ]
+	#dirs = [ v for v in glob.glob(path.join(curDir,diag,"*","*")) if path.isdir(v) ]
+	dirs = ["despiking_test"]
 	
 	# iterate through the subject directories
 	for d in dirs:
 		os.chdir(d)  # move in to subject's directory
+		#WBICNum = path.basename(d)
+		WBICNum = "10075"
 		f = open("submitScript.sh","wb")  # open a script to submit the preprocessing
 		f.writelines('\n'.join(["#!/bin/bash","#$ -cwd","#$ -l qname=clusterall.q","#$ -M "+email,"#$ -V"])) # headers required for the grid engine
-		f.writelines("\nfslreorient2std functional.nii functional_reordered.nii\n") # reorientate the functional image so that it applies skull strip and displays correctly
-		f.writelines("\nfslreorient2std structural.nii structural_reordered.nii\n") # reorientate structural image
-		f.writelines("\n3dWarp -prefix structural_reordered_deob.nii -deoblique structural_reordered.nii\n") # deoblique the structural image
+		f.writelines("\nfslreorient2std "+WBICNum+"_functional.nii "+WBICNum+"_functional_reordered.nii.gz\n") # reorientate the functional image so that it applies skull strip and displays correctly
+		f.writelines("\nfslreorient2std "+WBICNum+"_structural.nii "+WBICNum+"_structural_reordered.nii.gz\n") # reorientate structural image
+		f.writelines("\n3dWarp -prefix "+WBICNum+"_structural_reordered_deob.nii.gz -deoblique "+WBICNum+"_structural_reordered.nii.gz\n") # deoblique the structural image
 		
 		# speedypp with options - these may need changing, see signal processing toolbox for details
-		f.writelines("\n~/fmri_spt/speedypp.py -d functional_reordered.nii -a structural_reordered.nii -o -f 8mm --rall --skullstrip --atrophy --betthresh=0.2 --tpattern=seqminus --highpass=0.01 --despike --TR=2 --basetime=10 --OVERWRITE\n")
+		f.writelines("\n~/fmri_spt/speedypp.py -d "+WBICNum+"_functional_reordered.nii.gz -a "+WBICNum+"_structural_reordered.nii.gz -o --rall --tpattern=seqminus --TR=2 --basetime=10 --OVERWRITE\n")
+		
+		# wavelet despiking
+
+
+		# High pass filter
+
+
 		f.close() # close submission file
 		os.system("qsub submitScript.sh") # submit submision file to the grid engine
 		os.chdir(curDir) # move back to the main directory
