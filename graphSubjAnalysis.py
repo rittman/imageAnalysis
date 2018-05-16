b707dfb9376717dde800e8daf8ac961ded559d05
Timothy Rittman <tr332@medschl.cam.ac.uk>
2018-05-16 11:55:15 +0100

    Minor updates to graph analysis file. Added extractCentres script

--- graphSubjAnalysis.py
+++ graphSubjAnalysis.py
@@ -20,7 +20,7 @@ edgePCCons = [v for v in range(1,11)]
 #excludedNodes = [28, 303, 355, 339, 131, 250, 491, 205, 423, 140, 434, 142, 235,
 #                 244, 493, 21, 26, 232, 76, 234, 422] # nodes with insufficient coverage
 
-excludedNodes = []
+excludedNodes = [21, 26, 28, 35, 54, 73, 75, 76, 129, 133, 135, 140, 142, 160, 169, 177, 189, 232, 234, 235, 244, 245, 294, 304, 322, 339, 363, 386, 396, 419, 422, 423, 434, 476, 481, 493]
 
 parcelFile = "parcel_500.txt"
 thresholdtype = "local"
